3b380b7c02d88a6f783ff136b1d54ceb2ceb4edc
Steven Allen <sallen@tractionguest.com>
2019-12-14 07:34:51 +0000

    Deleting useless yaml

--- lib/generators/sidekiq_autoscale/install/install_generator.rb
+++ lib/generators/sidekiq_autoscale/install/install_generator.rb
@@ -7,7 +7,6 @@ module SidekiqAutoscale
     source_root File.expand_path("templates", __dir__)
 
     def generate_install
-      template "sidekiq_autoscale_yaml_template.template", "config/sidekiq_autoscale.yml"
       template "sidekiq_autoscale_initializer_template.template", "config/initializers/sidekiq_autoscale.rb"
     end
   end
--- lib/sidekiq_autoscale/railtie.rb
+++ lib/sidekiq_autoscale/railtie.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require "rails"
-require "sidekiq_autoscale/config/file_loader"
 
 module SidekiqAutoscale
   class Railtie < ::Rails::Railtie
@@ -10,9 +9,6 @@ module SidekiqAutoscale
     initializer "sidekiq_autoscale.configure", after: :load_config_initializers do |_app|
       env = ENV["RAILS_ENV"] || ENV["RACK_ENV"] || "development"
 
-      %w[config/sidekiq_autoscale.yml.erb config/sidekiq_autoscale.yml].find do |filename|
-        SidekiqAutoscale::Config::FileLoader.load(filename, env)
-      end
     end
   end
 end
